9e3e8a169c50087c9598116c8d0191b0e4aa7d91
Tuan Anh Nguyen <>
2025-08-07 14:40:38 +0700

    start project

---
+++ XcodeCloudTest/XcodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestApp.swift
+//  XcodeCloudTest
+//
+//  Created by SB15803 on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeCloudTestTests/XcodeCloudTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestTests.swift
+//  XcodeCloudTestTests
+//
+//  Created by SB15803 on 7/8/25.
+//
+
+import Testing
+@testable import XcodeCloudTest
+
+struct XcodeCloudTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
