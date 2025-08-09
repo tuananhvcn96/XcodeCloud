09a61e618bc4b7bbfdd17380c5abaaebdc5c06ef
Tuan Anh Nguyen <tuananh5871@gmail.com>
2025-08-10 03:14:43 +0700

    remove Text

--- XcodeCloudTest/ContentView.swift
+++ XcodeCloudTest/ContentView.swift
@@ -18,14 +18,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.blue)
             Text("Hello, world!")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.blue)
-            Text("Hello, world!")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.blue)
-            Text("Hello, world!")
         }
         .padding()
     }
